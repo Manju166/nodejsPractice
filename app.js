c9b610ad4b906c526be6a2d785dd6505812365dc
Manju166 <manjubhandariabc@gmail.com>
2024-04-20 11:33:41 +0545

    complete ui

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
+// const app = require("express")
 const express = require('express')
 const app = express()
+
+app.set('view engine','ejs')
+
 app.get('/',(req,res)=>{
-    res.send("Started node js")
+    res.render("home")
+})
+app.get("/blog",(req,res)=>{
+    res.render("blog")
+})
+app.get("/blog/create",(req,res)=>{
+    res.render("createblog")
+})
+app.get("/blog/edit",(req,res)=>{
+    res.render("editblog")
 })
 
 app.listen(3000,()=>{
